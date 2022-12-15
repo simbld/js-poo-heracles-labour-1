c0f95208447020649d7b7b0f406f4e2f5d15c92e
SIMON BULLADO <s_bullado51@hotmail.com>
2022-12-16 00:36:39 +0100

    Create fonction isAlive

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require("./src/Fighter.js");
+
+const FighterMan = new Fighter("🧔 Heracles the Man", 20, 6);
+const NemeanLion = new Fighter("🦁 Nemean the Lion", 11, 13);
+
+let round = 0;
